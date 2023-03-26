e9e23f0e23583c22dca71e98abd95e6da852a93a
DGJ Pasqualini <d.g.j.pasqualini@student.tudelft.nl>
2023-03-26 11:44:30 +0200

    Made function include capital, now it works fine

--- load_sets.py
+++ load_sets.py
@@ -2,15 +2,15 @@ import pandas as pd
 import os
 
 
-def load_ft_set():
+def load_Ft_set():
     this_directory = os.path.dirname(os.path.abspath(__file__))
-    ft_usable = pd.read_csv(os.path.join(this_directory, 'Ft_set.csv'), index_col=0)
+    Ft_usable = pd.read_csv(os.path.join(this_directory, 'Ft_set.csv'), index_col=0)
 
-    return ft_usable
+    return Ft_usable
 
 
-def load_d_set():
+def load_D_set():
     this_directory = os.path.dirname(os.path.abspath(__file__))
-    d_usable = pd.read_csv(os.path.join(this_directory, 'D_set.csv'), index_col=0)
+    D_usable = pd.read_csv(os.path.join(this_directory, 'D_set.csv'), index_col=0)
 
-    return d_usable
+    return D_usable
