cc5a6ab0a1533a3ea0fe8409fa7a85acfb026897
DGJ Pasqualini <d.g.j.pasqualini@student.tudelft.nl>
2023-03-28 14:25:16 +0200

    Made a file called: self_made including al functio

--- function_files/return_X_y.py
+++
@@ -1,9 +0,0 @@
-
-
-def return_X_y(df):
-    '''
-    Returns dataframe y_d including the labels, and a seperate dataframe X_d including the feature information.
-    '''
-    y_d = df.pop('label').to_frame()
-    X_d = df
-    return X_d, y_d
---
+++ function_files/self_made.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+import numpy as np
+import matplotlib as plt
+
+from load_sets import load_D_set
+from sklearn.feature_selection import VarianceThreshold
+
+
+def return_X_y(df):
+    '''
+    Returns dataframe y_d including the labels, and a seperate dataframe X_d including the feature information.
+    '''
+    y_d = df.pop('label').to_frame()
+    X_d = df
+    return X_d, y_d
+
+
+def removal_zero_var(feature_df):
+    '''
+    This function will remove zero-variance features from the dataset
+    '''
+    filter = VarianceThreshold(threshold=0.0)
+    df_filter = filter.fit_transform(feature_df)
+    features_kept = filter.get_support()
+    names = feature_df.columns[features_kept]
+    df_filter = pd.DataFrame(df_filter, columns=names)
+
+    return df_filter
