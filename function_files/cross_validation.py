38ec2ef78e0894ba27d6d0f9eeeccff6e2aca4fc
DGJ Pasqualini <d.g.j.pasqualini@student.tudelft.nl>
2023-03-28 18:35:31 +0200

    Cross-validation has been finished.

--- function_files/cross_validation.py
+++ function_files/cross_validation.py
@@ -3,9 +3,6 @@ import os
 import numpy as np
 import matplotlib as plt
 
-from sklearn.model_selection import train_test_split
-
-
 from load_sets import load_D_set
 from self_made import return_X_y, removal_zero_var
 
@@ -19,28 +16,34 @@ def cross_validation(data_set):
     consisting out of 90% and 10% of the design set, respectively.
     '''
 
-    for i in range(1, 8):
+    for i in range(1, 12):
         X_Va_s = "X_Vs_s" + str(i)
         X_Tr_s = "X_Tr_s" + str(i)
+        X_Tr_s2 = "X_Tr_s2" + str(i)
 
         if i == 1:
-            X_Va_s = data_set.iloc[:(i*int(0.09*len(data_set))+i)]
-            X_Tr_s = data_set.iloc[i*int(0.09*len(data_set))+i:]  # Deze term moet nu verder uitgewerkt worden!
+            X_Va_s = data_set.iloc[: (i*int(0.09*len(data_set))+i)]
+            X_Tr_s = data_set.iloc[(i*int(0.09*len(data_set))+i):]
+
         elif 1 < i and i <= 5:
-            X_Va_s = data_set.iloc[((i-1)*int(0.09*len(data_set))+(i-1)):(i*int(0.09*len(data_set))+i)]
-            X_Tr_s = data_set.iloc[i*int(0.09*len(data_set)):]  # Deze term moet nu verder uitgewerkt worden!
+            X_Va_s = data_set.iloc[((i-1)*int(0.09*len(data_set))+(i-1)): (i*int(0.09*len(data_set))+i)]
+            X_Tr_s = data_set.iloc[: ((i-1)*int(0.09*len(data_set))+(i-1))]
+            X_Tr_s2 = data_set.iloc[(i*int(0.09*len(data_set))+i):]
+            X_Tr_s = pd.concat([X_Tr_s, X_Tr_s2], ignore_index=False)  # Deze term moet nu verder uitgewerkt worden!
+
         else:
-            X_Va_s = data_set.iloc[5+(i-1)*int(0.09*len(data_set)):5+i*int(0.09*len(data_set))]
-            X_Tr_s = data_set.iloc[i*int(0.09*len(data_set)):]  # Deze term moet nu verder uitgewerkt worden!
+            X_Va_s = data_set.iloc[((i-1)*int(0.09*len(data_set))+5): (i*int(0.09*len(data_set))+5)]
+            X_Tr_s = data_set.iloc[: (((i-1)*int(0.09*len(data_set))+5))]
+            X_Tr_s2 = data_set.iloc[(i*int(0.09*len(data_set))+5):]
+            X_Tr_s = pd.concat([X_Tr_s, X_Tr_s2], ignore_index=False)  # Deze term moet nu verder uitgewerkt worden!
 
         this_directory = os.path.dirname(os.path.abspath(__file__))
         root = os.path.dirname(this_directory)
         data_folder = os.path.join(root, 'datasets')
-        X_Tr_s.to_csv(os.path.join(data_folder, 'X_Tr_s{}.csv'.format(i)))
-        X_Va_s.to_csv(os.path.join(data_folder, 'X_Va_s{}.csv'.format(i)))
+        data_folder_Tr = os.path.join(data_folder, 'X_Tr_sets')
+        data_folder_Va = os.path.join(data_folder, 'X_Va_sets')
+        X_Tr_s.to_csv(os.path.join(data_folder_Tr, 'X_Tr_s{}.csv'.format(i)))
+        X_Va_s.to_csv(os.path.join(data_folder_Va, 'X_Va_s{}.csv'.format(i)))
 
-        if i == 10:
+        if i == 11:
             print(f'{i*2} Files have been created')
-
-
-cross_validation(D_s)
