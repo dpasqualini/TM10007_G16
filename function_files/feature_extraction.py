31f824bd0f72d5041ebee377922f8bb4c1c4f732
Britt Sticker <b.j.j.sticker@student.tudelft.nl>
2023-03-28 15:11:17 +0200

    Creating a file that can test ftre extraction mthd

--- function_files/feature_extraction.py
+++ function_files/feature_extraction.py
@@ -3,4 +3,10 @@ import os
 import numpy as np
 import matplotlib as plt
 
-from sklearn.feature_selection import VarianceThreshold
+from load_sets import load_D_set
+from self_made import return_X_y, removal_zero_var
+
+D_s = load_D_set()
+X_d, y_d = return_X_y(D_s)
+
+print(X_d.head())
