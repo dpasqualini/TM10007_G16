cce7e0b42a540015d7186a38ecebfe5d8c487bc7
DGJ Pasqualini <d.g.j.pasqualini@student.tudelft.nl>
2023-03-30 14:30:35 +0200

    Added functions to load specific Va and Tr set

--- function_files/self_made.py
+++ function_files/self_made.py
@@ -105,6 +105,34 @@ def create_cross_val_sets(data_set, labels):
             print(f'{i*2*2} Files have been created, in folder "datasets"')
 
 
+def load_Tr_set(n):
+    X_Tr_s = "X_Tr_s" + str(n)
+    y_Tr_s = "y_Tr_s" + str(n)
+
+    this_directory = os.path.dirname(os.path.abspath(__file__))
+    root = os.path.dirname(this_directory)
+    data_folder = os.path.join(root, 'datasets')
+    data_folder_Tr = os.path.join(data_folder, 'tr_sets')
+    X_Tr_s = pd.read_csv(os.path.join(data_folder_Tr, 'X_Tr_s{}.csv'.format(n)), index_col=0)
+    y_Tr_s = pd.read_csv(os.path.join(data_folder_Tr, 'y_Tr_s{}.csv'.format(n)), index_col=0)
+
+    return X_Tr_s, y_Tr_s
+
+
+def load_Va_set(n):
+    X_Va_s = "X_Va_s" + str(n)
+    y_Va_s = "y_Va_s" + str(n)
+
+    this_directory = os.path.dirname(os.path.abspath(__file__))
+    root = os.path.dirname(this_directory)
+    data_folder = os.path.join(root, 'datasets')
+    data_folder_Tr = os.path.join(data_folder, 'va_sets')
+    X_Va_s = pd.read_csv(os.path.join(data_folder_Tr, 'X_Va_s{}.csv'.format(n)), index_col=0)
+    y_Va_s = pd.read_csv(os.path.join(data_folder_Tr, 'y_Va_s{}.csv'.format(n)), index_col=0)
+
+    return X_Va_s, y_Va_s
+
+
 def normalize_column(column):
     '''
     This function will normalize a panda Series using the minimum-maximum scaling.
