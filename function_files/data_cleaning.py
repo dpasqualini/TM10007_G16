d974eff5b52fea403f6e485dd333391cbd2d8311
Britt Sticker <b.j.j.sticker@student.tudelft.nl>
2023-03-30 16:44:44 +0200

    one def with preprocessed  data (Nan, var,Zero)

--- function_files/data_cleaning.py
+++ function_files/data_cleaning.py
@@ -41,18 +41,55 @@ def removal_zeros(feature_df):
     return feature_df, zero_cols
 
 X_d_remove_zeroperc, zero_cols = removal_zeros(X_d)
-print(f"amount of columns containing {len(zero_cols)} and there called {zero_cols}")
-print(X_d.loc[:, zero_cols[0]])
+print(f"amount of columns containing zero are {len(zero_cols)} and there called {zero_cols}")
+print(X_d[zero_cols[4]])
 print(X_d_remove_zeroperc)
 
-# def removal_NaNs(feature_df):
-#     zero_percents = (feature_df == NaN).sum() / len(feature_df) * 100
-#     threshold = 50
-#     nonzero_cols = feature_df.columns[zero_percents < threshold]
-#     zero_cols = feature_df.columns[zero_percents > threshold]
-#     feature_df = feature_df.loc[:, nonzero_cols]
-#     return feature_df, zero_cols
+def removal_nans(feature_df):
+    nan_percents = feature_df.isna().sum() / len(feature_df) * 100
+    threshold_nan = 50
+    nonnan_cols = feature_df.columns[nan_percents < threshold_nan]
+    nan_cols = feature_df.columns[nan_percents > threshold_nan]
+    feature_df = feature_df.loc[:, nonnan_cols]
+    return feature_df, nan_cols
 
-# X_d_remove_zeroperc, zero_cols = removal_zeros(X_d)
-# print(f"amount of columns containing {len(zero_cols)} and there called {zero_cols}")
-# print(X_d_remove_zeroperc)
+# checking whether the NaNs are found like this
+# X_d_nan = X_d.copy()
+# X_d_nan['PREDICT_original_sf_compactness_avg_2.5D'] = np.nan
+X_d_remove_nanperc, nan_cols = removal_nans(X_d)
+print(f"amount of columns containing NaN are {len(nan_cols)} and there called {nan_cols}")
+print(X_d_remove_nanperc)
+
+def preprocessing(feature_df):
+    '''
+    This function preprocesses the features by removing zero-variance, zero-filled, and nan-filled features from the dataset.
+    '''
+    filter = VarianceThreshold(threshold=0.0)
+    df_filter = filter.fit_transform(feature_df)
+    features_kept = filter.get_support()
+    names = feature_df.columns[features_kept]
+    #df_filter = pd.DataFrame(df_filter, columns=names)
+
+    # remove zero-filled features (if >50% is zero)
+    zero_percents = (feature_df == 0).sum() / len(feature_df) * 100
+    threshold = 50
+    nonzero_cols = feature_df.columns[zero_percents < threshold]
+    zero_cols = feature_df.columns[zero_percents > threshold]
+
+    # remove nan-filled features (if >50% is NaN)
+    nan_percents = feature_df.isna().sum() / len(feature_df) * 100
+    threshold_nan = 50
+    nonnan_cols = feature_df.columns[nan_percents < threshold_nan]
+    nan_cols = feature_df.columns[nan_percents > threshold_nan]
+
+    common_cols = set(nonnan_cols).intersection(set(nonzero_cols)).intersection(set(names))
+    feature_df_chang = feature_df[list(common_cols)]
+
+
+    return feature_df_chang, nan_cols, zero_cols, names
+
+X_d_preprocessed, nan_cols, zero_cols, names = preprocessing(X_d)
+print(f"The new preprocessed dataset is {X_d_preprocessed}")
+print(f"The columns with >50% NaN are {nan_cols}")
+print(f"The columns with >50% zero are {zero_cols}")
+print(f"The columns with zero variance are {names}")
