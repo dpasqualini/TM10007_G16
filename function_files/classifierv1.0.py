5465b25305e329300bff373f3c2122391d71a241
Britt Sticker <b.j.j.sticker@student.tudelft.nl>
2023-04-08 17:12:01 +0200

    Lot of visualizations of the PCA

--- function_files/classifierv1.0.py
+++ function_files/classifierv1.0.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import numpy as np
+import seaborn as sns
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from scipy.stats import ttest_ind
@@ -24,7 +25,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.kernel_approximation import RBFSampler
 from sklearn import metrics
 
-
 # Load the data
 X_train, y_train = load_Tr_set(1)
 y_train = y_train['label'].replace({'benign': 0, 'malignant': 1})
@@ -35,14 +35,102 @@ X_train_n = X_train.apply(normalize_column)
 X_train_n = X_train_n.to_numpy()
 
 # Perform PCA
-pca = PCA(n_components=33)
+pca = PCA(n_components=31) # 33 is choosen as higher than 90 percent variance suggests taking n_components of 33
 X_pca = pca.fit_transform(X_train_n)
 
 # Visualize the results
-plt.scatter(X_pca[:, 0], X_pca[:, 1], c=y_train, cmap='viridis')
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
-#plt.show()
+# plt.scatter(X_pca[:, 0], X_pca[:, 1], c=y_train, cmap='viridis')
+# plt.xlabel('Principal Component 1')
+# plt.ylabel('Principal Component 2')
+# #plt.show()
+
+# get basic info
+n_components = len(pca.explained_variance_ratio_)
+explained_variance = pca.explained_variance_ratio_
+cum_explained_variance = np.cumsum(explained_variance)
+idx = np.arange(n_components)+1
+
+df_explained_variance = pd.DataFrame([explained_variance, cum_explained_variance], 
+                                     index=['explained variance', 'cumulative'], 
+                                     columns=idx).T
+
+mean_explained_variance = df_explained_variance.iloc[:,0].mean() # calculate mean explained variance
+
+# DISPLAY info about PCs
+print('PCA Overview')
+print('='*40)
+print("Total: {} components".format(n_components))
+print('-'*40)
+print('Mean explained variance:', round(mean_explained_variance,3))
+print('-'*40)
+print(df_explained_variance)
+print('-'*40)
+
+#limit plot to x PC
+limit = int(input("Limit scree plot to nth component (0 for all) > "))
+if limit > 0:
+    limit_df = limit
+else:
+    limit_df = n_components
+
+df_explained_variance_limited = df_explained_variance.iloc[:limit_df,:]
+
+#make scree plot
+fig, ax1 = plt.subplots(figsize=(15,6))
+
+ax1.set_title('Explained variance across principal components', fontsize=14)
+ax1.set_xlabel('Principal component', fontsize=12)
+ax1.set_ylabel('Explained variance', fontsize=12)
+
+ax2 = sns.barplot(x=idx[:limit_df], y='explained variance', data=df_explained_variance_limited, palette='summer')
+ax2 = ax1.twinx()
+ax2.grid(False)
+
+ax2.set_ylabel('Cumulative', fontsize=14)
+ax2 = sns.lineplot(x=idx[:limit_df]-1, y='cumulative', data=df_explained_variance_limited, color='#fc8d59')
+
+ax1.axhline(mean_explained_variance, ls='--', color='#fc8d59') #plot mean
+ax1.text(-.8, mean_explained_variance+(mean_explained_variance*.05), "average", color='#fc8d59', fontsize=14) #label y axis
+
+max_y1 = max(df_explained_variance_limited.iloc[:,0])
+max_y2 = max(df_explained_variance_limited.iloc[:,1])
+ax1.set(ylim=(0, max_y1+max_y1*.1))
+ax2.set(ylim=(0, max_y2+max_y2*.1))
+
+plt.show()
+
+# ### Factor loadings!! ###
+selected_features = X_train.columns
+print(selected_features)
+
+############### DEFINE PARAMS
+top_k = 3
+#select data based on percentile (top_q) or top-k features
+top_q = .50
+top_pc = 3
+###############
+
+# PCA factor loadings
+df_c = pd.DataFrame(pca.components_, columns=selected_features).T
+
+print("Factor Loadings for the 1. component \n(explains {0:.2f} of the variance)".format(df_explained_variance.iloc[0,0]))
+print('='*40,'\n')
+print('Top {} highest'.format(top_k))
+print('-'*40)
+print(df_c.iloc[:,0].sort_values(ascending=False)[:top_k], '\n')
+
+print('Top {} lowest'.format(top_k))
+print('-'*40)
+print(df_c.iloc[:,0].sort_values()[:top_k])
+
+# Plot heatmap
+size_yaxis = round(X_train[selected_features].shape[1] * 0.5)
+# Create a mask of values above 0.025
+mask = df_c > 0.025
+
+fig, ax = plt.subplots(figsize=(8,size_yaxis))
+sns.heatmap(df_c.iloc[:,:top_pc], annot=True, cmap="YlGnBu", ax=ax)
+plt.show()
 
 X_ben = X_pca[y_train == 0]
 X_mal = X_pca[y_train == 1]
