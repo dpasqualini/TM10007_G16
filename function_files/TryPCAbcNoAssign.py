02a3c0e7b6583b2a59d3f2350b8b319e04040814
Britt Sticker <b.j.j.sticker@student.tudelft.nl>
2023-04-11 10:21:02 +0200

    updated assignment w clsfs & pca

--- function_files/TryPCAbcNoAssign.py
+++ function_files/TryPCAbcNoAssign.py
@@ -17,143 +17,143 @@ from sklearn.ensemble import RandomForestClassifier # Not regressor as it is for
 
 ### in selfmad
 
-def PCA_info_notransform(X):
-    # Covariance matrix
-    covar_matrix = PCA(n_components = len(X))
-    covar_matrix.fit(X)
-    eigenvalues = covar_matrix.explained_variance_
+# def PCA_info_notransform(X):
+#     # Covariance matrix
+#     covar_matrix = PCA(n_components = len(X))
+#     covar_matrix.fit(X)
+#     eigenvalues = covar_matrix.explained_variance_
 
-    variance = covar_matrix.explained_variance_ratio_
-    var = np.cumsum(np.round(variance, decimals=3)*100)
+#     variance = covar_matrix.explained_variance_ratio_
+#     var = np.cumsum(np.round(variance, decimals=3)*100)
 
-    return variance, var
+#     return variance, var
 
-def PCA_plot(var):
-    plt.ylabel('% Variance Explained')
-    plt.xlabel('# of Features')
-    plt.title('PCA Variance Explained')
-    plt.ylim(min(var), 100.5)
-    plt.style.context('seaborn-whitegrid')
-    plt.axhline(y=90, color='r', linestyle='--')
-    plt.plot(var)
-    plt.show()
+# def PCA_plot(var):
+#     plt.ylabel('% Variance Explained')
+#     plt.xlabel('# of Features')
+#     plt.title('PCA Variance Explained')
+#     plt.ylim(min(var), 100.5)
+#     plt.style.context('seaborn-whitegrid')
+#     plt.axhline(y=90, color='r', linestyle='--')
+#     plt.plot(var)
+#     plt.show()
     
-    for j, val in enumerate(var):
-        if val > 90:
-            print("The index of the first number that is bigger than 90% is:", j)
-            n_comp = j+1
-            break
-    return n_comp
+#     for j, val in enumerate(var):
+#         if val > 90:
+#             print("The index of the first number that is bigger than 90% is:", j)
+#             n_comp = j+1
+#             break
+#     return n_comp
 
-def PCA_transform(X_tr_normalized, n_comp):
-    pca = PCA(n_components=n_comp) # as index starts at 0 the number of components is one higher
-    X_pca = pca.fit_transform(X_tr_normalized)
+# def PCA_transform(X_tr_normalized, n_comp):
+#     pca = PCA(n_components=n_comp) # as index starts at 0 the number of components is one higher
+#     X_pca = pca.fit_transform(X_tr_normalized)
 
-    eigenvalues = pca.explained_variance_
+#     eigenvalues = pca.explained_variance_
 
-    variance = pca.explained_variance_ratio_
-    var = np.cumsum(np.round(variance, decimals=3)*100)
+#     variance = pca.explained_variance_ratio_
+#     var = np.cumsum(np.round(variance, decimals=3)*100)
 
-    return pca, X_pca, variance, var
+#     return pca, X_pca, variance, var
 
-def PCA_transform_overview(variance, var):
-    n_components = len(variance)
-    idx = np.arange(n_components)+1
+# def PCA_transform_overview(variance, var):
+#     n_components = len(variance)
+#     idx = np.arange(n_components)+1
 
-    df_explained_variance = pd.DataFrame([variance, var], 
-                                        index=['explained variance', 'cumulative'], 
-                                        columns=idx).T
+#     df_explained_variance = pd.DataFrame([variance, var], 
+#                                         index=['explained variance', 'cumulative'], 
+#                                         columns=idx).T
 
-    mean_explained_variance = df_explained_variance.iloc[:,0].mean() # calculate mean explained variance
+#     mean_explained_variance = df_explained_variance.iloc[:,0].mean() # calculate mean explained variance
 
-    # DISPLAY info about PCs
-    print('PCA Overview')
-    print('='*40)
-    print("Total: {} components".format(n_components))
-    print('-'*40)
-    print('Mean explained variance:', round(mean_explained_variance,3))
-    print('-'*40)
-    print(df_explained_variance)
-    print('-'*40)
-    return df_explained_variance, mean_explained_variance, idx
+#     # DISPLAY info about PCs
+#     print('PCA Overview')
+#     print('='*40)
+#     print("Total: {} components".format(n_components))
+#     print('-'*40)
+#     print('Mean explained variance:', round(mean_explained_variance,3))
+#     print('-'*40)
+#     print(df_explained_variance)
+#     print('-'*40)
+#     return df_explained_variance, mean_explained_variance, idx
 
-def scree_plot(df_explained_variance, mean_explained_variance, idx):
-    limit_df = 15 # change this number if you want to have more or less PCA components visualized
-    df_explained_variance_limited = df_explained_variance.iloc[:limit_df,:]
+# def scree_plot(df_explained_variance, mean_explained_variance, idx):
+#     limit_df = 15 # change this number if you want to have more or less PCA components visualized
+#     df_explained_variance_limited = df_explained_variance.iloc[:limit_df,:]
 
-    #make scree plot
-    fig, ax1 = plt.subplots(figsize=(15,6))
+#     #make scree plot
+#     fig, ax1 = plt.subplots(figsize=(15,6))
 
-    ax1.set_title('Explained variance across principal components', fontsize=14)
-    ax1.set_xlabel('Principal component', fontsize=12)
-    ax1.set_ylabel('Explained variance', fontsize=12)
+#     ax1.set_title('Explained variance across principal components', fontsize=14)
+#     ax1.set_xlabel('Principal component', fontsize=12)
+#     ax1.set_ylabel('Explained variance', fontsize=12)
 
-    ax2 = sns.barplot(x=idx[:limit_df], y='explained variance', data=df_explained_variance_limited, palette='summer')
-    ax2 = ax1.twinx()
-    ax2.grid(False)
+#     ax2 = sns.barplot(x=idx[:limit_df], y='explained variance', data=df_explained_variance_limited, palette='summer')
+#     ax2 = ax1.twinx()
+#     ax2.grid(False)
 
-    ax2.set_ylabel('Cumulative', fontsize=14)
-    ax2 = sns.lineplot(x=idx[:limit_df]-1, y='cumulative', data=df_explained_variance_limited, color='#fc8d59')
+#     ax2.set_ylabel('Cumulative', fontsize=14)
+#     ax2 = sns.lineplot(x=idx[:limit_df]-1, y='cumulative', data=df_explained_variance_limited, color='#fc8d59')
 
-    ax1.axhline(mean_explained_variance, ls='--', color='#fc8d59') #plot mean
-    ax1.text(-.8, mean_explained_variance+(mean_explained_variance*.05), "average", color='#fc8d59', fontsize=14) #label y axis
+#     ax1.axhline(mean_explained_variance, ls='--', color='#fc8d59') #plot mean
+#     ax1.text(-.8, mean_explained_variance+(mean_explained_variance*.05), "average", color='#fc8d59', fontsize=14) #label y axis
 
-    max_y1 = max(df_explained_variance_limited.iloc[:,0])
-    max_y2 = max(df_explained_variance_limited.iloc[:,1])
-    ax1.set(ylim=(0, max_y1+max_y1*.1))
-    ax2.set(ylim=(0, max_y2+max_y2*.1))
+#     max_y1 = max(df_explained_variance_limited.iloc[:,0])
+#     max_y2 = max(df_explained_variance_limited.iloc[:,1])
+#     ax1.set(ylim=(0, max_y1+max_y1*.1))
+#     ax2.set(ylim=(0, max_y2+max_y2*.1))
     
-    return plt.show()
+#     return plt.show()
 
-def factor_loadings(X, pca, df_explained_variance):
-    ############### DEFINE PARAMETERS 
-    selected_features = X.columns
-    top_k = 3
-    #select data based on percentile (top_q) or top-k features
-    top_q = .50
-    top_pc = 3
-    ###############
+# def factor_loadings(X, pca, df_explained_variance):
+#     ############### DEFINE PARAMETERS 
+#     selected_features = X.columns
+#     top_k = 3
+#     #select data based on percentile (top_q) or top-k features
+#     top_q = .50
+#     top_pc = 3
+#     ###############
 
-    # PCA factor loadings
-    df_c = pd.DataFrame(pca.components_, columns=selected_features).T
+#     # PCA factor loadings
+#     df_c = pd.DataFrame(pca.components_, columns=selected_features).T
 
-    print("Factor Loadings for the 1. component \n(explains {0:.2f} of the variance)".format(df_explained_variance.iloc[0,0]))
-    print('='*40,'\n')
-    print('Top {} highest'.format(top_k))
-    print('-'*40)
-    print(df_c.iloc[:,0].sort_values(ascending=False)[:top_k], '\n')
+#     print("Factor Loadings for the 1. component \n(explains {0:.2f} of the variance)".format(df_explained_variance.iloc[0,0]))
+#     print('='*40,'\n')
+#     print('Top {} highest'.format(top_k))
+#     print('-'*40)
+#     print(df_c.iloc[:,0].sort_values(ascending=False)[:top_k], '\n')
 
-    print('Top {} lowest'.format(top_k))
-    print('-'*40)
-    print(df_c.iloc[:,0].sort_values()[:top_k])
+#     print('Top {} lowest'.format(top_k))
+#     print('-'*40)
+#     print(df_c.iloc[:,0].sort_values()[:top_k])
 
-    # Plot heatmap
-    size_yaxis = round(X[selected_features].shape[1] * 0.5)
-    # Create a mask of values above 0.025
-    mask = df_c > 0.025
+#     # Plot heatmap
+#     size_yaxis = round(X[selected_features].shape[1] * 0.5)
+#     # Create a mask of values above 0.025
+#     mask = df_c > 0.025
 
-    fig, ax = plt.subplots(figsize=(8,size_yaxis))
-    sns.heatmap(df_c.iloc[:,:top_pc], annot=True, cmap="YlGnBu", ax=ax)
-    return plt.show()
+#     fig, ax = plt.subplots(figsize=(8,size_yaxis))
+#     sns.heatmap(df_c.iloc[:,:top_pc], annot=True, cmap="YlGnBu", ax=ax)
+#     return plt.show()
 
-def classifiers(clsfs, X_pca, y_train):
-    # Loop over each classifier and fit the model, make predictions and compute metrics
-    for clf in clsfs:
-        clf.fit(X_pca, y_train)
-        y_pred = clf.predict(X_pca)
-        accuracy = metrics.accuracy_score(y_train, y_pred)
-        F1 = metrics.f1_score(y_train, y_pred)
-        precision = metrics.precision_score(y_train, y_pred)
-        recall = metrics.recall_score(y_train, y_pred)
-        print(type(clf).__name__)
-        print('Accuracy:', accuracy)
-        print('F1:', F1)
-        print('Precision:', precision)
-        print('Recall:', recall)
-        print()
-    return
+# def classifiers(clsfs, X_pca, y_train):
+#     # Loop over each classifier and fit the model, make predictions and compute metrics
+#     for clf in clsfs:
+#         clf.fit(X_pca, y_train)
+#         y_pred = clf.predict(X_pca)
+#         accuracy = metrics.accuracy_score(y_train, y_pred)
+#         F1 = metrics.f1_score(y_train, y_pred)
+#         precision = metrics.precision_score(y_train, y_pred)
+#         recall = metrics.recall_score(y_train, y_pred)
+#         print(type(clf).__name__)
+#         print('Accuracy:', accuracy)
+#         print('F1:', F1)
+#         print('Precision:', precision)
+#         print('Recall:', recall)
+#         print()
+#     return
 
-###
+# ###
 
 # Load the data
 X_train, y_train = load_Tr_set(1)
@@ -165,7 +165,8 @@ X_train_n = X_train.apply(normalize_column)
 X_train_n = X_train_n.to_numpy()
 
 # To get info about whole dataset in PCA
-variance1, var1 = PCA_info_notransform(X_train_n)
+selected_features = X_train_n
+variance1, var1 = PCA_info_notransform(X_train_n, selected_features)
 n_comp = PCA_plot(var1)
 # To get the new X_pca dataset
 pca, X_pca, variance, var = PCA_transform(X_train_n, n_comp)
--- function_files/self_made.py
+++ function_files/self_made.py
@@ -3,11 +3,13 @@ import os
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.manifold import TSNE
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.feature_selection import VarianceThreshold
+from sklearn import metrics
 
 
 def load_Ft_set():
@@ -236,3 +238,143 @@ def plt_lda(df_features, labels):
     plt.ylim(-0.1, 0.1)
 
     return plt.show()
+
+
+def PCA_info_notransform(X, selected_features):
+    # Covariance matrix
+    covar_matrix = PCA(n_components = len(selected_features))
+    covar_matrix.fit(X)
+    eigenvalues = covar_matrix.explained_variance_
+
+    variance = covar_matrix.explained_variance_ratio_
+    var = np.cumsum(np.round(variance, decimals=3)*100)
+
+    return variance, var
+
+def PCA_plot(var):
+    plt.ylabel('% Variance Explained')
+    plt.xlabel('# of Features')
+    plt.title('PCA Variance Explained')
+    plt.ylim(min(var), 100.5)
+    plt.style.context('seaborn-whitegrid')
+    plt.axhline(y=90, color='r', linestyle='--')
+    plt.plot(var)
+    plt.show()
+    
+    for j, val in enumerate(var):
+        if val > 90:
+            print("The index of the first number that is bigger than 90% is:", j)
+            n_comp = j+1
+            break
+    return n_comp
+
+def PCA_transform(X_tr_normalized, n_comp):
+    pca = PCA(n_components=n_comp) # as index starts at 0 the number of components is one higher
+    X_pca = pca.fit_transform(X_tr_normalized)
+
+    eigenvalues = pca.explained_variance_
+
+    variance = pca.explained_variance_ratio_
+    var = np.cumsum(np.round(variance, decimals=3)*100)
+
+    return pca, X_pca, variance, var
+
+def PCA_transform_overview(variance, var):
+    n_components = len(variance)
+    idx = np.arange(n_components)+1
+
+    df_explained_variance = pd.DataFrame([variance, var], 
+                                        index=['explained variance', 'cumulative [%]'], 
+                                        columns=idx).T
+
+    mean_explained_variance = df_explained_variance.iloc[:,0].mean() # calculate mean explained variance
+
+    # DISPLAY info about PCs
+    print('PCA Overview')
+    print('='*40)
+    print("Total: {} components".format(n_components))
+    print('-'*40)
+    print('Mean explained variance:', round(mean_explained_variance,3))
+    print('-'*40)
+    print(df_explained_variance)
+    print('-'*40)
+    return df_explained_variance, mean_explained_variance, idx
+
+def scree_plot(df_explained_variance, mean_explained_variance, idx):
+    limit_df = 15 # change this number if you want to have more or less PCA components visualized
+    df_explained_variance_limited = df_explained_variance.iloc[:limit_df,:]
+
+    #make scree plot
+    fig, ax1 = plt.subplots(figsize=(15,6))
+
+    ax1.set_title('Explained variance across principal components', fontsize=14)
+    ax1.set_xlabel('Principal component', fontsize=12)
+    ax1.set_ylabel('Explained variance', fontsize=12)
+
+    ax2 = sns.barplot(x=idx[:limit_df], y='explained variance', data=df_explained_variance_limited, palette='summer')
+    ax2 = ax1.twinx()
+    ax2.grid(False)
+
+    ax2.set_ylabel('Cumulative', fontsize=14)
+    ax2 = sns.lineplot(x=idx[:limit_df]-1, y='cumulative [%]', data=df_explained_variance_limited, color='#fc8d59')
+
+    ax1.axhline(mean_explained_variance, ls='--', color='#fc8d59') #plot mean
+    ax1.text(-.8, mean_explained_variance+(mean_explained_variance*.05), "average", color='#fc8d59', fontsize=14) #label y axis
+
+    max_y1 = max(df_explained_variance_limited.iloc[:,0])
+    max_y2 = max(df_explained_variance_limited.iloc[:,1])
+    ax1.set(ylim=(0, max_y1+max_y1*.1))
+    ax2.set(ylim=(0, max_y2+max_y2*.1))
+    
+    return plt.show()
+
+def factor_loadings(X, pca, df_explained_variance):
+    ############### DEFINE PARAMETERS 
+    selected_features = X.columns
+    top_k = 3
+    #select data based on percentile (top_q) or top-k features
+    top_q = .50
+    top_pc = 3
+    ###############
+
+    # PCA factor loadings
+    df_c = pd.DataFrame(pca.components_, columns=selected_features).T
+
+    print("Factor Loadings for the 1. component \n(explains {0:.2f} of the variance)".format(df_explained_variance.iloc[0,0]))
+    print('='*40,'\n')
+    print('Top {} highest'.format(top_k))
+    print('-'*40)
+    print(df_c.iloc[:,0].sort_values(ascending=False)[:top_k], '\n')
+
+    print('Top {} lowest'.format(top_k))
+    print('-'*40)
+    print(df_c.iloc[:,0].sort_values()[:top_k])
+
+    # Plot heatmap
+    size_yaxis = round(X[selected_features].shape[1] * 0.5)
+    # Create a mask of values above 0.025
+    mask = df_c > 0.025
+
+    fig, ax = plt.subplots(figsize=(8,size_yaxis))
+    sns.heatmap(df_c.iloc[:,:], annot=True, cmap="YlGnBu", ax=ax)
+    return plt.show()
+
+def classifiers(clsfs, X_pca, y_train):
+    f1_scores = []
+    # Loop over each classifier and fit the model, make predictions and compute metrics
+    for clf in clsfs:
+        clf.fit(X_pca, y_train)
+        y_pred = clf.predict(X_pca)
+        accuracy = metrics.accuracy_score(y_train, y_pred)
+        F1 = metrics.f1_score(y_train, y_pred)
+        precision = metrics.precision_score(y_train, y_pred)
+        recall = metrics.recall_score(y_train, y_pred)
+        print(type(clf).__name__)
+        print('Accuracy:', accuracy)
+        print('F1:', F1)
+        f1_scores.append(F1)
+        print('Precision:', precision)
+        print('Recall:', recall)
+        print()
+    return f1_scores
+
